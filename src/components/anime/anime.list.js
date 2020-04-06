042fadf6a1b93a6637c4f35c3a879b5c8d08931f
Joey Robinson <theamazingness@gmail.com>
2020-04-06 02:51:25 -0400

    more styling.

--- src/components/anime/anime.list.js
+++ src/components/anime/anime.list.js
@@ -1,29 +1,31 @@
 import React from "react"
 import Link from "next/link"
-import Spinner from "../spinner"
 import { LazyLoadImage } from "react-lazy-load-image-component"
+import Spinner from "../spinner"
 
-const AnimeList = ({ href, as, mal_id, image_url, url, title, synopsis }) => (
-  <li key={mal_id} className="search--container search--li li">
-    <div className="li--title">
-      <h5>{title}</h5>
-    </div>
-    <div className="li--image">
-      <a href={url} target="_blank" rel="noopner noreferrer">
-        <LazyLoadImage
-          alt={title}
-          src={image_url} // use normal <img> attributes as props
-        />
-      </a>
-    </div>
-    <p>{!synopsis ? "No synopsis is available for this title" : synopsis}</p>
+const AnimeList = ({ href, as, mal_id, image_url, url, title, synopsis }) => {
+  return (
+    <li key={mal_id} className="search--container search--li li">
+      <div className="li--title">
+        <h5>{title}</h5>
+      </div>
+      <div className="li--image">
+        <a href={url} target="_blank" rel="noopner noreferrer">
+          {!image_url ? (
+            <Spinner />
+          ) : (
+            <LazyLoadImage alt={title} src={image_url} />
+          )}
+        </a>
+      </div>
+      <p>{!synopsis ? "No synopsis is available for this title" : synopsis}</p>
 
-    <div className="li--link">
-      <Link href={href} as={as}>
-        <a>Read More about it here.</a>
-      </Link>
-    </div>
-  </li>
-)
-
+      <div className="li--link">
+        <Link href={href} as={as}>
+          <a>Read More about it here.</a>
+        </Link>
+      </div>
+    </li>
+  )
+}
 export default AnimeList
