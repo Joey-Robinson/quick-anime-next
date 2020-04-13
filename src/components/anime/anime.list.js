9f7621723c6d75e8aed17d65399a785d6e4acebc
Joey Robinson <theamazingness@gmail.com>
2020-04-13 04:08:37 -0400

    Styling changes still. Adjusting the results from search. Need to fix padding.

--- src/components/anime/anime.list.js
+++ src/components/anime/anime.list.js
@@ -8,15 +8,30 @@ const AnimeList = ({ href, as, mal_id, image_url, url, title, synopsis }) => {
       <div className="li--title">
         <h5>{title}</h5>
       </div>
-      <div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "13rem 1fr",
+          gridTemplateRows: "auto 1fr",
+        }}
+      >
         <LazyLoadImage alt={title} src={image_url} />
-      </div>
-      <p>{!synopsis ? "No synopsis is available for this title" : synopsis}</p>
-
-      <div className="li--link">
-        <Link href={href} as={as}>
-          <a>Read More about it here.</a>
-        </Link>
+        <p>
+          {!synopsis ? "No synopsis is available for this title" : synopsis}
+        </p>
+        <div
+          style={{
+            gridColumn: "2",
+            gridRow: "-1",
+            justifySelf: "center",
+            alignSelf: "start",
+            padding: "0 0 3rem 0",
+          }}
+        >
+          <Link href={href} as={as}>
+            <a>Read More about it here.</a>
+          </Link>
+        </div>
       </div>
     </li>
   )
