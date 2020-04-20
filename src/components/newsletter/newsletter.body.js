e51670415b035372dc49d5709f67718e96ba00d2
Joey Robinson <theamazingness@gmail.com>
2020-04-19 22:15:05 -0400

    image to blog post

--- src/components/newsletter/newsletter.body.js
+++ src/components/newsletter/newsletter.body.js
@@ -1,12 +1,8 @@
 import React from "react"
-import Link from "next/link"
 
 const PostBody = ({ content }) => {
   return (
     <>
-      <Link href="/newsletter">
-        <a title="Newsletter Page">&larr; Back to Newsletter</a>
-      </Link>
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </>
   )
