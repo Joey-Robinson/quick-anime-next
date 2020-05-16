34bb0c150f649fc07a14da072253f3d04b6520c6
Joey Robinson <theamazingness@gmail.com>
2020-05-16 16:24:14 -0400

    rolled back base/base change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,35 +4,6 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <style global jsx>
-          {`
-            *,
-            *::before,
-            *::after {
-              margin: 0;
-              padding: 0;
-              box-sizing: inherit;
-            }
-
-            html {
-              box-sizing: border-box;
-              font-size: 62.5%;
-            }
-
-            body {
-              overflow-x: hidden;
-              scroll-behavior: smooth;
-              font-weight: 300;
-              font-family: "Roboto", sans-serif;
-              line-height: 1.4;
-              height: 100vh;
-              min-height: 100vh;
-              background: #15202b;
-              color: white;
-            }
-          `}
-        </style>
-
         <Head />
         <body>
           <Main />
