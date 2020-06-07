b7965373a96a7aba0cb62c5f3ed1781bde90d796
Joey Robinson <theamazingness@gmail.com>
2020-06-07 12:48:46 -0400

    newsletter looks okish for now. header time

--- src/components/newsletter/newsletter.preview.js
+++ src/components/newsletter/newsletter.preview.js
@@ -3,27 +3,29 @@ import React, { memo } from "react"
 import CoverImage from "./newsletter.coverimage"
 import Date from "./newsletter.date"
 
-const PostPreview = memo(({ tags, title, coverImage, date, excerpt, slug }) => {
-  const shortenedExcerpt = excerpt.slice(0, 150)
-  return (
-    <li key={title} className="blog--li news">
-      <h2>
-        <Link as={`/newsletter/${slug}`} href="/newsletter/[slug]">
-          <a>{title}</a>
-        </Link>
-      </h2>
-      <div style={{ textAlign: "center", padding: "1.5rem 0" }}>
-        <Date dateString={date} />
-      </div>
-      <div className="news--image">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <div className="news--excerpt">
-        <p>{shortenedExcerpt}</p>
-      </div>
-      <ul>{tags}</ul>
-    </li>
-  )
-})
+const PostPreview = memo(
+  ({ specialKey, tags, title, coverImage, date, excerpt, slug }) => {
+    const shortenedExcerpt = excerpt.slice(0, 150)
+    return (
+      <li key={title} className="blog--li news">
+        <h2>
+          <Link as={`/newsletter/${slug}`} href="/newsletter/[slug]">
+            <a>{title}</a>
+          </Link>
+        </h2>
+        <div style={{ textAlign: "center", padding: "1.5rem 0" }}>
+          <Date dateString={date} />
+        </div>
+        <div className="news--image">
+          <CoverImage slug={slug} title={title} src={coverImage} />
+        </div>
+        <div className="news--excerpt">
+          <p>{shortenedExcerpt}</p>
+        </div>
+        <ul key={specialKey}>{tags}</ul>
+      </li>
+    )
+  }
+)
 
 export default PostPreview
