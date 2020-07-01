c4fd920df640f93901b78e98d5cf8bb2ee37f067
Joey Robinson <theamazingness@gmail.com>
2020-07-01 14:35:14 -0400

    styling

--- src/components/global/global.layout.js
+++ src/components/global/global.layout.js
@@ -4,9 +4,7 @@ import Header from "../header/header.main"
 const Layout = ({ children, className = "children" }) => (
   <main className="primary">
     <Header />
-    <section className={className}>
-      <div className="container">{children}</div>
-    </section>
+    <section className={className}>{children}</section>
   </main>
 )
 
