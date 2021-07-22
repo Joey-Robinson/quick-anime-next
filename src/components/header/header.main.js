5d06f6a8ca70746749deceff9a87c0a8a9b0f84b
Joey Robinson <theamazingness@gmail.com>
2021-07-21 23:29:26 -0400

    removing all the damn things

--- src/components/header/header.main.js
+++ src/components/header/header.main.js
@@ -1,15 +1,11 @@
-import React from "react"
-import { slide as Menu } from "react-burger-menu"
 import Navigation from "./header.nav"
 import Notification from "./header.notification"
 
 const Header = () => {
   return (
     <header>
-      <Menu>
-        <Navigation />
-        <Notification />
-      </Menu>
+      <Navigation />
+      <Notification />
     </header>
   )
 }
--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,90 +1,90 @@
-import React, { useState } from "react"
-import Layout from "../components/global/global.layout"
-import GlobalSearch from "../components/global/global.search"
-import SEO from "../components/global/global.seo"
-import { getAllPosts } from "../components/newsletter/newsletter.api"
-import PostPreview from "../components/newsletter/newsletter.preview"
+// import React, { useState } from "react"
+// import Layout from "../components/global/global.layout"
+// import GlobalSearch from "../components/global/global.search"
+// import SEO from "../components/global/global.seo"
+// import { getAllPosts } from "../components/newsletter/newsletter.api"
+// import PostPreview from "../components/newsletter/newsletter.preview"
 
-// Use this for blog images. 4mb for 1 page, disgusting.
-const Newsletter = ({ allPosts }) => {
-  const morePosts = allPosts
-  const emptyQuery = ""
-  const [filteredPosts, setFilteredPosts] = useState({
-    filteredData: [],
-    query: emptyQuery,
-  })
+// // Use this for blog images. 4mb for 1 page, disgusting.
+// const Newsletter = ({ allPosts }) => {
+//   const morePosts = allPosts
+//   const emptyQuery = ""
+//   const [filteredPosts, setFilteredPosts] = useState({
+//     filteredData: [],
+//     query: emptyQuery,
+//   })
 
-  const handleInputChange = (event) => {
-    const query = event.target.value
-    const posts = morePosts || []
-    const filteredData = posts.filter((post) => {
-      const { title, tags } = post
-      return (
-        title.toLowerCase().includes(query.toLowerCase()) ||
-        tags.join("").toLowerCase().includes(query.toLowerCase())
-      )
-    })
+//   const handleInputChange = (event) => {
+//     const query = event.target.value
+//     const posts = morePosts || []
+//     const filteredData = posts.filter((post) => {
+//       const { title, tags } = post
+//       return (
+//         title.toLowerCase().includes(query.toLowerCase()) ||
+//         tags.join("").toLowerCase().includes(query.toLowerCase())
+//       )
+//     })
 
-    setFilteredPosts({
-      query,
-      filteredData,
-    })
-  }
-  const { filteredData, query } = filteredPosts
-  const hasSearchResults = filteredData && query !== emptyQuery
-  const posts = hasSearchResults ? filteredData : allPosts
+//     setFilteredPosts({
+//       query,
+//       filteredData,
+//     })
+//   }
+//   const { filteredData, query } = filteredPosts
+//   const hasSearchResults = filteredData && query !== emptyQuery
+//   const posts = hasSearchResults ? filteredData : allPosts
 
-  return (
-    <Layout className="post--news">
-      <SEO title="Newsletter" description="Monthly Anime Musings" />
-      <GlobalSearch
-        onSubmit={(event) => event.preventDefault()}
-        value={query}
-        onChange={handleInputChange}
-        placeholder="Search Newsletters"
-        id="newsletter--search"
-        spanText="Search Newsletters"
-        ariaLabel="Search Newsletters"
-        label="Search Newsletters"
-        idFor="newsletter--search"
-        submitStyle={{ display: "none" }}
-      />
-      <div className="container">
-        <ul className="blog">
-          {posts &&
-            posts.map(({ tags, title, date, excerpt, coverImage, slug }) => {
-              const tagList = tags.map((tag) => <li>{tag}</li>)
-              return (
-                <PostPreview
-                  tags={tagList}
-                  key={slug}
-                  title={title}
-                  date={date}
-                  excerpt={excerpt}
-                  coverImage={coverImage}
-                  slug={slug}
-                />
-              )
-            })}
-        </ul>
-      </div>
-    </Layout>
-  )
-}
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-    "tags",
-  ])
+//   return (
+//     <Layout className="post--news">
+//       <SEO title="Newsletter" description="Monthly Anime Musings" />
+//       <GlobalSearch
+//         onSubmit={(event) => event.preventDefault()}
+//         value={query}
+//         onChange={handleInputChange}
+//         placeholder="Search Newsletters"
+//         id="newsletter--search"
+//         spanText="Search Newsletters"
+//         ariaLabel="Search Newsletters"
+//         label="Search Newsletters"
+//         idFor="newsletter--search"
+//         submitStyle={{ display: "none" }}
+//       />
+//       <div className="container">
+//         <ul className="blog">
+//           {posts &&
+//             posts.map(({ tags, title, date, excerpt, coverImage, slug }) => {
+//               const tagList = tags.map((tag) => <li>{tag}</li>)
+//               return (
+//                 <PostPreview
+//                   tags={tagList}
+//                   key={slug}
+//                   title={title}
+//                   date={date}
+//                   excerpt={excerpt}
+//                   coverImage={coverImage}
+//                   slug={slug}
+//                 />
+//               )
+//             })}
+//         </ul>
+//       </div>
+//     </Layout>
+//   )
+// }
+// export async function getStaticProps() {
+//   const allPosts = getAllPosts([
+//     "title",
+//     "date",
+//     "slug",
+//     "author",
+//     "coverImage",
+//     "excerpt",
+//     "tags",
+//   ])
 
-  return {
-    props: { allPosts },
-  }
-}
+//   return {
+//     props: { allPosts },
+//   }
+// }
 
-export default Newsletter
+// export default Newsletter
