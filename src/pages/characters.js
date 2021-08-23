49573fa564a547fcaaa5ff1842f8d65a769b7c4b
Joey Robinson <theamazingness@gmail.com>
2021-08-23 11:13:29 -0400

    styling for characters

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { useEffect, useState } from "react"
 import Layout from "../components/global/global.layout"
 import SEO from "../components/global/global.seo"
@@ -6,9 +7,7 @@ const Characters = () => {
   const [popularCharacters, setPopularCharacters] = useState({ top: [] })
 
   const upcomingCall = async () => {
-    const response = await fetch(
-      "https://api.jikan.moe/v3/top/anime/1/bypopularity"
-    )
+    const response = await fetch("https://api.jikan.moe/v3/top/characters")
     const data = await response.json()
     setPopularCharacters(data)
   }
@@ -23,7 +22,35 @@ const Characters = () => {
       <SEO title="Characters" />
       <div className="characters">
         <div className="characters--search">Search</div>
-        From Characters
+        <div className="characters--container ul chars">
+          <div className="chars--rank">Rank</div>
+          <div className="chars--character">Character</div>
+          <div className="chars--animeography">Animeography</div>
+          <div className="chars--mangaography">Mangaography</div>
+          <div className="chars--favorites">Favorites</div>
+        </div>
+        <ul className="ul--main">
+          {popularCharacters.top
+            .slice(0, 1)
+            .map(({ mal_id, rank, title, url, image_url, name_kanji }) => (
+              <li>
+                <span>{rank}</span>
+                <div>
+                  <Image
+                    width={50}
+                    height={78}
+                    src={image_url}
+                    alt={`Thumb for ${title}`}
+                    layout="fixed"
+                  />
+                </div>
+                <hgroup>
+                  <h4>{title}</h4>
+                  <h5>{name_kanji}</h5>
+                </hgroup>
+              </li>
+            ))}
+        </ul>
       </div>
     </Layout>
   )
---
+++ src/pages/characters/[id].js
@@ -0,0 +1,6 @@
+// Post.getInitialProps = async function (context) {
+//   const { id } = context.query
+//   const result = await fetch(`      https://api.jikan.moe/v3/character/1`)
+//   const anime = await result.json()
+//   return { anime }
+// }
