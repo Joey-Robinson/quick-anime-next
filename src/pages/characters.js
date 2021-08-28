b986f98ffba96980b892f46e7b50222e8e2bbddd
Joey Robinson <theamazingness@gmail.com>
2021-08-28 11:30:53 -0400

    characters page

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,27 +1,70 @@
 import Image from "next/image"
+import Link from "next/link"
 import { useEffect, useState } from "react"
 import Layout from "../components/global/global.layout"
 import SEO from "../components/global/global.seo"
+import { trimString } from "../utils/trim.utils"
 
 const Characters = () => {
   const [popularCharacters, setPopularCharacters] = useState({ top: [] })
+  const [nextPage, setNextPage] = useState(1)
 
-  const upcomingCall = async () => {
-    const response = await fetch("https://api.jikan.moe/v3/top/characters")
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  const characterCall = async () => {
+    const response = await fetch(
+      `https://api.jikan.moe/v3/top/characters/${nextPage}`
+    )
     const data = await response.json()
     setPopularCharacters(data)
   }
 
+  // const nextFifty = async () => {
+  //   const response = await fetch(
+  //     `https://api.jikan.moe/v3/top/characters/${nextPage}`
+  //   )
+  //   const data = await response.json()
+  //   setPopularCharacters(data)
+  //   setNextPage(nextPage + 1)
+  //   console.log(nextPage)
+  // }
+
+  // const previousFifty = async () => {
+  //   const response = await fetch(
+  //     `https://api.jikan.moe/v3/top/characters/${nextPage}`
+  //   )
+  //   const data = await response.json()
+  //   setPopularCharacters(data)
+  //   setNextPage(nextPage - 1)
+  // }
+
   useEffect(() => {
-    upcomingCall()
-  }, [])
+    characterCall()
+  }, [nextPage])
 
   // console.log(popularCharacters.top)
   return (
     <Layout>
       <SEO title="Characters" />
       <div className="characters">
+        <ul className="characters--alphabet">
+          {alphabet.split("").map((l) => (
+            <li>{l}</li>
+          ))}
+        </ul>
         {/* <div className="characters--search">Search</div> */}
+        <div className="characters--heading pagination">
+          <h2>Most Favorited</h2>
+          {/* {nextPage <= 1 ? (
+            ""
+          ) : (
+            <button onClick={previousFifty}>Prev 50</button>
+          )}
+          <button onClick={nextFifty} className="pagination--next">
+            Next 50 &gt;
+          </button> */}
+          <div className="pagination--next">Next 50 &gt;</div>
+        </div>
         <div className="characters--container ul">
           <div className="chars--rank  chars">Rank</div>
           <div className="chars--character  chars">Character</div>
@@ -30,54 +73,68 @@ const Characters = () => {
           <div className="chars--favorites  chars">Favorites</div>
         </div>
         <ul className="ul--main">
-          {popularCharacters.top
-            .slice(0, 1)
-            .map(
-              ({
-                mal_id,
-                rank,
-                title,
-                url,
-                image_url,
-                name_kanji,
-                animeography,
-                mangaography,
-                favorites,
-              }) => {
-                const animeCatalog = animeography.map(({ name }) => (
-                  <p>{name}</p>
-                ))
-                const mangaCatalog = mangaography.map(({ name }) => (
-                  <p>{name}</p>
-                ))
-                const favoriteValue = favorites
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                // console.log(animeCatalog)
+          {popularCharacters.top.map(
+            ({
+              mal_id,
+              rank,
+              title,
+              url,
+              image_url,
+              name_kanji,
+              animeography,
+              mangaography,
+              favorites,
+            }) => {
+              const animeCatalog = animeography.map(({ name }) => {
+                const shortName = trimString(name, 22)
                 return (
-                  <li className="char">
-                    <span className="char--rank">{rank}</span>
-                    <div className="char--container">
-                      <Image
-                        width={50}
-                        height={78}
-                        src={image_url}
-                        alt={`Thumb for ${title}`}
-                        layout="fixed"
-                      />
-                      <hgroup>
-                        <h4>{title}</h4>
-                        <h5>{name_kanji}</h5>
-                      </hgroup>
-                    </div>
-                    <div className="char--animeography">{animeCatalog}</div>
-                    <div className="char--mangaography">{mangaCatalog}</div>
-                    <div className="char--favorites">{favoriteValue}</div>
-                  </li>
+                  <div>
+                    <Link href={`/anime/[id]/`} as={`/anime/${mal_id}/`}>
+                      {shortName}
+                    </Link>
+                  </div>
                 )
-              }
-            )}
+              })
+              const mangaCatalog = mangaography.map(({ name }) => {
+                const shortName = trimString(name, 23)
+                return (
+                  <>
+                    <Link href={`/anime/[id]/`} as={`/anime/${mal_id}/`}>
+                      {shortName}
+                    </Link>
+                  </>
+                )
+              })
+              const favoriteValue = favorites
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              // console.log(animeCatalog)
+              return (
+                <li className="char">
+                  <span className="char--rank">{rank}</span>
+                  <div className="char--image">
+                    <Image
+                      width={50}
+                      height={78}
+                      src={image_url}
+                      alt={`Thumb for ${title}`}
+                      layout="fixed"
+                    />
+                  </div>
+                  <hgroup className="char--name">
+                    <h4>{title}</h4>
+                    <h5>{name_kanji}</h5>
+                  </hgroup>
+
+                  <div className="char--animeography">{animeCatalog}</div>
+                  <div className="char--mangaography">{mangaCatalog}</div>
+                  <div className="char--favorites">{favoriteValue}</div>
+                </li>
+              )
+            }
+          )}
         </ul>
+        {/* <button onClick={() => setNextPage(nextPage + 1)}>Next 50 &gt;</button> */}
       </div>
     </Layout>
   )
