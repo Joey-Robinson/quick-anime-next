6a16144f09986fb0bffe1f9c807926fc4431716a
Joey Robinson <theamazingness@gmail.com>
2021-09-07 06:10:14 -0400

    hoping this doesn't break anything xd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import { AnimatePresence } from "framer-motion"
 import "react-lazy-load-image-component/src/effects/blur.css"
 import "tailwindcss/tailwind.css"
 import "../../next-seo.config"
-import "../styles/base.css"
 import "../styles/main.scss"
 
 export default function MyApp({ Component, pageProps }) {
